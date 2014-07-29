b9cac9df6889912fdab0bd7f85cc9fe2d9fe1fba
Patrick Gerdsmeier <patrick.gerdsmeier@gmail.com>
2014-07-29 11:31:00 -0700

    It's done - xfce looks like 2014 =P

---
+++ xfwm4/README.md
@@ -0,0 +1,2 @@
+# Platt
+Platt is a flat window theme for xfwm4. Enjoy =) 
---
+++ xfwm4/mk.sh
@@ -0,0 +1,23 @@
+cp title-1-active.png title-2-active.png
+cp title-1-active.png title-3-active.png
+cp title-1-active.png title-4-active.png
+cp title-1-active.png title-5-active.png
+cp title-1-active.png top-left-active.png
+cp title-1-active.png top-right-active.png
+
+cp title-1-inactive.png title-2-inactive.png
+cp title-1-inactive.png title-3-inactive.png
+cp title-1-inactive.png title-4-inactive.png
+cp title-1-inactive.png title-5-inactive.png
+cp title-1-inactive.png top-left-inactive.png
+cp title-1-inactive.png top-right-inactive.png
+
+cp bottom-active.png left-active.png
+cp bottom-active.png right-active.png
+cp bottom-active.png bottom-left-active.png
+cp bottom-active.png bottom-right-active.png
+
+cp bottom-inactive.png left-inactive.png
+cp bottom-inactive.png right-inactive.png
+cp bottom-inactive.png bottom-left-inactive.png
+cp bottom-inactive.png bottom-right-inactive.png
